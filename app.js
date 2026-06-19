72440af096cdfe99ca815189fbbdc2848a9c1965
上野 知樹 Tomoki Ueno <00056270@bandainamco.co.jp>
2026-06-19 16:24:32 +0900

    index.jsの内容をapp.jsに移動

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ app.use(express.json());
 // ルーティングの設定
 const authRoutes = require('./routes/auth');
 const itemRoutes = require('./routes/items');
+const apiRoutes = require('./routes/api');
 app.use('/auth', authRoutes);
 app.use('/items', itemRoutes);
+app.use('/api', apiRoutes);
+
+// UI（index.jsにあったやつ）テストコード
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>APIテスト</h1>
+    <button onclick="callApi()">API呼び出し</button>
+    <pre id="result"></pre>
+
+    <script>
+      async function callApi() {
+        const res = await fetch('/api/hello');
+        const data = await res.json();
+        document.getElementById('result').innerText = JSON.stringify(data, null, 2);
+      }
+    </script>
+  `);
+});
 
 // 起動
 const PORT = process.env.PORT || 3000;
--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+consle.log('index.js loaded');
+require('./app');
 
-// POST受け取れるようにする
-app.use(express.json());
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-const apiRoutes = require('./routes/api');
-app.use('/api', apiRoutes);
+// // POST受け取れるようにする
+// app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  //res.send('Hello from Azure!');
-  res.send(`
-    <h1>APIテスト</h1>
-    <button onclick="callApi()">API呼び出し</button>
-    <pre id="result"></pre>
-
-    <script>
-      async function callApi() {
-        const res = await fetch('/api/hello');
-        const data = await res.json();
-        document.getElementById('result').innerText = JSON.stringify(data, null, 2);
-      }
-    </script>
-  `);
-});
+// const apiRoutes = require('./routes/api');
+// app.use('/api', apiRoutes);
 
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.get('/', (req, res) => {
+//   //res.send('Hello from Azure!');
+//   res.send(`
+//     <h1>APIテスト</h1>
+//     <button onclick="callApi()">API呼び出し</button>
+//     <pre id="result"></pre>
+
+//     <script>
+//       async function callApi() {
+//         const res = await fetch('/api/hello');
+//         const data = await res.json();
+//         document.getElementById('result').innerText = JSON.stringify(data, null, 2);
+//       }
+//     </script>
+//   `);
+// });
+
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
